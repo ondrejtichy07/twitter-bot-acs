c9c3d9328fcd45c6d6cd93b2b80664bc66b274cf
Ondřej <ondrejtichy@ondrejs-macbookpro.local>
2019-09-01 20:57:47 +0200

    init

---
+++ daysSinceTitle.py
@@ -0,0 +1,36 @@
+import tweepy
+import time
+import sys
+sys.path.insert(1, '/Users/mac/Documents/PythonProjects/SMTP')
+import emailSender
+import datetime
+import os
+
+# Env variables
+CONSUMER_KEY = os.environ.get('CONSUMER_KEY_TWITTER')
+CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET_TWITTER')
+ACCESS_KEY = os.environ.get('ACCESS_KEY_TWITTER')
+ACCESS_SECRET = os.environ.get('ACCESS_SECRET_TWITTER')
+
+# Authentication
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+while True:
+    today = datetime.date.today()
+    b = datetime.date(2014, 5, 31)
+    diff = today - b
+    days = diff.days
+
+    print(days)
+    try:
+        api.update_status('{}'.format(days))
+        print("Success")
+    except Exception as err:
+        print(err)
+        emailSender.sendAlert(
+            'ERR', 'Check Twitter pyBot on Raspberry Pi!', 'ondrejtichy07@gmail.com')
+        pass
+
+    time.sleep(86400)
